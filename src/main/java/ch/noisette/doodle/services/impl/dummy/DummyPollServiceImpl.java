4baf827c3a5097fafef8bb852259bf0d95045d01
Johannes Eifert <johannes.eifert@unifr.ch>
2013-05-08 02:40:18 +0200

    get all polls implemented

--- src/main/java/ch/noisette/doodle/services/impl/dummy/DummyPollServiceImpl.java
+++ src/main/java/ch/noisette/doodle/services/impl/dummy/DummyPollServiceImpl.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Service;
 import ch.noisette.doodle.domains.Poll;
 import ch.noisette.doodle.domains.Subscriber;
 import ch.noisette.doodle.services.PollService;
-import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.logging.Level;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
@@ -28,23 +30,29 @@ public class DummyPollServiceImpl implements PollService {
         private final static int DB_PORT = 9160;
         private final static String UTF8 = "UTF-8";
         private Cassandra.Client client;
-        private TTransport tr
-        private boolean isConnected = false;
+        private TTransport tr;
         
 
 	private static final Logger logger_c = Logger.getLogger(DummyPollServiceImpl.class);
         
-        private synchronized void openDBconnection()throws TTransportException,
-                UnsupportedEncodingException, InvalidRequestException, 
-                NotFoundException, UnavailableException, TimedOutException, 
-                TException, AuthenticationException, AuthorizationException {
+        private synchronized void openDBconnection(){
             
             TTransport tr = new TFramedTransport(new TSocket(DB_HOST, DB_PORT));
             TProtocol protocol  = new TBinaryProtocol(tr);
             Cassandra.Client client = new Cassandra.Client(protocol);
-            tr.open();
-            client.set_keyspace(DB_KEYSPACE);
-            isConnected = true;
+            try {
+                tr.open();
+            } catch (TTransportException ex) {
+                java.util.logging.Logger.getLogger(DummyPollServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                client.set_keyspace(DB_KEYSPACE);
+            } catch (InvalidRequestException ex) {
+                java.util.logging.Logger.getLogger(DummyPollServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (TException ex) {
+                java.util.logging.Logger.getLogger(DummyPollServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
         }
         
         private void closeDBconnection() {
@@ -59,8 +67,36 @@ public class DummyPollServiceImpl implements PollService {
 
 	@Override
 	public List<Poll> getAllPolls() {
-		// TODO Auto-generated method stub
-		return Collections.<Poll>emptyList();
+            List<Poll> polls = new LinkedList<Poll>();
+            openDBconnection();
+            KeyRange keyRange = new KeyRange(Integer.MAX_VALUE);
+            keyRange.setStart_key(new byte[0]);
+            keyRange.setEnd_key(new byte[0]);
+            
+            SlicePredicate slicePredicate = new SlicePredicate();
+            ColumnParent columnParent = new ColumnParent("stats");
+            
+            try {
+                List<KeySlice> keySlices = client.get_range_slices(columnParent, slicePredicate, keyRange, ConsistencyLevel.ONE);
+                
+                for (Iterator<KeySlice> iterator = keySlices.iterator(); iterator.hasNext(); ){
+                    KeySlice keySlice = iterator.next();
+                   polls.add(getPollById(new String(keySlice.getKey())));
+                }
+                
+            } catch (InvalidRequestException ex) {
+                java.util.logging.Logger.getLogger(DummyPollServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (UnavailableException ex) {
+                java.util.logging.Logger.getLogger(DummyPollServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (TimedOutException ex) {
+                java.util.logging.Logger.getLogger(DummyPollServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (TException ex) {
+                java.util.logging.Logger.getLogger(DummyPollServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            
+            closeDBconnection();
+            return polls;
 	}
 
 	@Override
